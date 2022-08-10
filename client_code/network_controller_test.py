4b5d1683087d2cdbabb06e53714cf1beb3f3e73b
hugetim@gmail.com <hugetim@gmail.com>
2022-08-10 15:23:43 +0000

    Edited network_controller_test

--- client_code/network_controller_test.py
+++ client_code/network_controller_test.py
@@ -5,5 +5,14 @@ from . import test_helper as th
 
 
 class ConnectionsTest(unittest.TestCase):
+  def setUp(self):
+    glob.connections = [
+      dict(user_id1='me', user_id2='o1'),
+      dict(user_id1='o1', user_id2='me'),
+    ]
+  
   def test_no_connections(self):
-    self.assertEqual(nc.get_connections(), [])
+    self.assertEqual(nc.get_connections('123'), [])
+
+  def test_my_connections(self):
+    self.assertEqual(len(nc.get_connections('me')), 1)
