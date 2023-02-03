84cd2e371536bc490e7161c3fe0251dec02b0eed
hugetim@gmail.com <hugetim@gmail.com>
2023-02-03 01:57:55 +0000

    Refactor request_test

--- server_code/request_test.py
+++ server_code/request_test.py
@@ -17,189 +17,207 @@ user3_id = USER3.get_id()
 
 u = sm.get_port_user(USER2, distance=0, simple=True)
 time1 = ProposalTime()
-port_prop1 = Proposal(user=u, min_size=3, max_size=10, 
-                      eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
-                      times=[time1])
-start_1 = sm.now() if time1.start_now else time1.start_date
-time2 = ProposalTime(start_date=start_1 + port.DEFAULT_NEXT_DELTA)
-port_prop2 = Proposal(user=u, min_size=2, max_size=3, 
-                      eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
-                      times=[time1, time2])
-port_prop_now = Proposal(user=u, min_size=2, max_size=3,
+prop_u2_3to10_in1hr = Proposal(
+  user=u, min_size=3, max_size=10, 
+  eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
+  times=[time1],
+)
+time2 = ProposalTime(start_date=time1.start_date + port.DEFAULT_NEXT_DELTA)
+prop_u2_2to3_in1hr_in2hr = Proposal(
+  user=u, min_size=2, max_size=3,
+  eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
+  times=[time1, time2],
+)
+prop_u2_2to3_now = Proposal(user=u, min_size=2, max_size=3,
                          eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
                          times=[ProposalTime(start_now=True)])
 
-o_port_prop1 = Proposal(user=sm.get_port_user(ADMIN, distance=0, simple=True), min_size=2, max_size=2,
-                   eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
-                   times=[time1])
-o_port_prop_now = Proposal(user=sm.get_port_user(ADMIN, distance=0, simple=True), min_size=2, max_size=3,
-                         eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
-                         times=[ProposalTime(start_now=True)])
-o_port_prop1_size3 = Proposal(user=sm.get_port_user(ADMIN, distance=0, simple=True), min_size=2, max_size=3,
-                   eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
-                   times=[time1])
+uA = sm.get_port_user(ADMIN, distance=0, simple=True)
+prop_uA_2to2_in1hr = Proposal(
+  user=uA, min_size=2, max_size=2,
+  eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
+  times=[time1],
+)
+prop_uA_2to3_now = Proposal(
+  user=uA, min_size=2, max_size=3,
+  eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
+  times=[ProposalTime(start_now=True)],
+)
+prop_uA_2to2_in1hr_size3 = Proposal(
+  user=uA, min_size=2, max_size=3,
+  eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
+  times=[time1],
+)
 
-o3_port_prop1 = Proposal(user=sm.get_port_user(USER3, distance=0, simple=True), min_size=3, max_size=10,
-                   eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
-                   times=[time1])
+prop_u3_3to10_in1hr = Proposal(
+  user=sm.get_port_user(USER3, distance=0, simple=True), min_size=3, max_size=10,
+  eligible=2, eligible_users=["u1"], eligible_groups=["g1"], eligible_starred=True,
+  times=[time1],
+)
 
 
-class TestNewRequest(unittest.TestCase):
+class TestNewRequests(unittest.TestCase):
   def test_new_single_later_request(self):
-    requests = tuple(ri._new_requests(user2_id, port_prop1))
+    prop = prop_u2_3to10_in1hr
+    requests = tuple(ri._new_requests(user2_id, prop))
     request = requests[0]
     # self.assertEqual(request.request_id, port_prop.times[0].time_id)
     # self.assertEqual(request.or_group_id, port_prop.prop_id)
     self.assertFalse(request.request_id)
     self.assertTrue(request.or_group_id)
-    self.assertEqual(request.eformat.duration, port_prop1.times[0].duration)
-    self.assertEqual(request.expire_dt, port_prop1.times[0].expire_date)
-    self.assertEqual(request.user, port_prop1.user)
-    self.assertEqual(request.start_dt, port_prop1.times[0].start_date)
+    self.assertEqual(request.eformat.duration, prop.times[0].duration)
+    self.assertEqual(request.expire_dt, prop.times[0].expire_date)
+    self.assertEqual(request.user, prop.user)
+    self.assertEqual(request.start_dt, prop.times[0].start_date)
     self.assertEqual(request.create_dt, request.edit_dt)
-    self.assertEqual(request.min_size, port_prop1.min_size)
-    self.assertEqual(request.max_size, port_prop1.max_size)
-    self.assertEqual(request.eligible, port_prop1.eligible)
-    self.assertEqual(request.eligible_users, port_prop1.eligible_users)
-    self.assertEqual(request.eligible_groups, port_prop1.eligible_groups)
-    self.assertEqual(request.eligible_starred, port_prop1.eligible_starred)
+    self.assertEqual(request.min_size, prop.min_size)
+    self.assertEqual(request.max_size, prop.max_size)
+    self.assertEqual(request.eligible, prop.eligible)
+    self.assertEqual(request.eligible_users, prop.eligible_users)
+    self.assertEqual(request.eligible_groups, prop.eligible_groups)
+    self.assertEqual(request.eligible_starred, prop.eligible_starred)
     self.assertEqual(request.current, True)
 
   def test_new_single_now_request(self):
-    requests = tuple(ri._new_requests(user2_id, port_prop_now))
+    prop = prop_u2_2to3_now
+    requests = tuple(ri._new_requests(user2_id, prop))
     request = requests[0]
     # self.assertEqual(request.request_id, port_prop.times[0].time_id)
     # self.assertEqual(request.or_group_id, port_prop.prop_id)
     self.assertFalse(request.request_id)
     self.assertTrue(request.or_group_id)
-    self.assertEqual(request.eformat.duration, port_prop_now.times[0].duration)
+    self.assertEqual(request.eformat.duration, prop.times[0].duration)
     self.assertEqual(request.expire_dt, request.start_dt + timedelta(seconds=p.WAIT_SECONDS))
-    self.assertEqual(request.user, port_prop_now.user)
+    self.assertEqual(request.user, prop.user)
     self.assertTrue(request.start_dt <= sm.now())
     self.assertEqual(request.create_dt, request.edit_dt)
-    self.assertEqual(request.min_size, port_prop_now.min_size)
-    self.assertEqual(request.max_size, port_prop_now.max_size)
-    self.assertEqual(request.eligible, port_prop_now.eligible)
-    self.assertEqual(request.eligible_users, port_prop_now.eligible_users)
-    self.assertEqual(request.eligible_groups, port_prop_now.eligible_groups)
-    self.assertEqual(request.eligible_starred, port_prop_now.eligible_starred)
+    self.assertEqual(request.min_size, prop.min_size)
+    self.assertEqual(request.max_size, prop.max_size)
+    self.assertEqual(request.eligible, prop.eligible)
+    self.assertEqual(request.eligible_users, prop.eligible_users)
+    self.assertEqual(request.eligible_groups, prop.eligible_groups)
+    self.assertEqual(request.eligible_starred, prop.eligible_starred)
     self.assertEqual(request.current, True)
     
   def test_new_multiple_later_requests(self):
-    requests = tuple(ri._new_requests(user2_id, port_prop2))
+    requests = tuple(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
     for i, request in enumerate(requests):
       # self.assertEqual(request.request_id, port_prop.times[i].time_id)
       # self.assertEqual(request.or_group_id, port_prop.prop_id)
       self.assertFalse(request.request_id)
       self.assertTrue(request.or_group_id)
-      self.assertEqual(request.eformat.duration, port_prop2.times[i].duration)
-      self.assertEqual(request.expire_dt, port_prop2.times[i].expire_date)
-      self.assertEqual(request.user, port_prop2.user)
-      self.assertEqual(request.start_dt, port_prop2.times[i].start_date)
+      self.assertEqual(request.eformat.duration, prop_u2_2to3_in1hr_in2hr.times[i].duration)
+      self.assertEqual(request.expire_dt, prop_u2_2to3_in1hr_in2hr.times[i].expire_date)
+      self.assertEqual(request.user, prop_u2_2to3_in1hr_in2hr.user)
+      self.assertEqual(request.start_dt, prop_u2_2to3_in1hr_in2hr.times[i].start_date)
       self.assertEqual(request.create_dt, request.edit_dt)
-      self.assertEqual(request.min_size, port_prop2.min_size)
-      self.assertEqual(request.max_size, port_prop2.max_size)
-      self.assertEqual(request.eligible, port_prop2.eligible)
-      self.assertEqual(request.eligible_users, port_prop2.eligible_users)
-      self.assertEqual(request.eligible_groups, port_prop2.eligible_groups)
-      self.assertEqual(request.eligible_starred, port_prop2.eligible_starred)
+      self.assertEqual(request.min_size, prop_u2_2to3_in1hr_in2hr.min_size)
+      self.assertEqual(request.max_size, prop_u2_2to3_in1hr_in2hr.max_size)
+      self.assertEqual(request.eligible, prop_u2_2to3_in1hr_in2hr.eligible)
+      self.assertEqual(request.eligible_users, prop_u2_2to3_in1hr_in2hr.eligible_users)
+      self.assertEqual(request.eligible_groups, prop_u2_2to3_in1hr_in2hr.eligible_groups)
+      self.assertEqual(request.eligible_starred, prop_u2_2to3_in1hr_in2hr.eligible_starred)
       self.assertEqual(request.current, True)
 
+
+class TestHaveConflicts(unittest.TestCase):
   def test_new_single_later_request_only_no_conflicts(self):
-    requests = tuple(ri._new_requests(user2_id, port_prop1))
+    requests = tuple(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
     self.assertFalse(rs.have_conflicts(requests))
 
   def test_new_single_later_request_conflict(self):
-    requests = list(ri._new_requests(user2_id, port_prop1))
-    requests.extend(ri._new_requests(user2_id, port_prop1))
+    requests = list(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
+    requests.extend(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
     self.assertTrue(rs.have_conflicts(requests))
 
   def test_new_multiple_later_requests_conflict(self):
-    requests = list(ri._new_requests(user2_id, port_prop2))
-    requests.extend(ri._new_requests(user2_id, port_prop1))
+    requests = list(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+    requests.extend(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
     self.assertTrue(rs.have_conflicts(requests))
 
-    requests = list(ri._new_requests(user2_id, port_prop1))
-    requests.extend(ri._new_requests(user2_id, port_prop2))
+    requests = list(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
+    requests.extend(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
     self.assertTrue(rs.have_conflicts(requests))
 
   def test_new_multiple_requests_no_conflict(self):
-    requests = list(ri._new_requests(user2_id, port_prop2))
-    requests.extend(ri._new_requests(user2_id, port_prop_now))
+    requests = list(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+    requests.extend(ri._new_requests(user2_id, prop_u2_2to3_now))
     self.assertFalse(rs.have_conflicts(requests))
 
-    requests = list(ri._new_requests(user2_id, port_prop_now))
-    requests.extend(ri._new_requests(user2_id, port_prop2))
+    requests = list(ri._new_requests(user2_id, prop_u2_2to3_now))
+    requests.extend(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
     self.assertFalse(rs.have_conflicts(requests))
 
   def test_new_single_later_request_only_no_conflicts(self):
-    requests = tuple(ri._new_requests(user2_id, port_prop1))
+    requests = tuple(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
     self.assertFalse(rs.have_conflicts(requests))
 
+
+class TestPotentialMatches(unittest.TestCase):
   def test_new_later_requests_no_match(self):
-    new_requests = tuple(ri._new_requests(user2_id, port_prop2))
-    o_requests = tuple(ri._new_requests(admin_id, o_port_prop_now))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+    o_requests = tuple(ri._new_requests(admin_id, prop_uA_2to3_now))
     self.assertFalse(rs.potential_matches(new_requests, o_requests))
 
-    new_requests = tuple(ri._new_requests(user2_id, port_prop1))
-    o_requests = tuple(ri._new_requests(admin_id, o_port_prop1))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
+    o_requests = tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr))
     self.assertFalse(rs.potential_matches(new_requests, o_requests))
 
   def test_new_later_requests_match(self):
-    new_requests = tuple(ri._new_requests(user2_id, port_prop2))
-    o_requests = tuple(ri._new_requests(admin_id, o_port_prop1))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+    o_requests = tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertIn({new_requests[0], o_requests[0]},
                   potential_matches)
     self.assertEqual(len(potential_matches), 1)
 
-    new_requests = tuple(ri._new_requests(user2_id, port_prop1))
-    o_requests = (tuple(ri._new_requests(admin_id, o_port_prop1))
-                  + tuple(ri._new_requests(user3_id, o3_port_prop1)))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
+    o_requests = (tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr))
+                  + tuple(ri._new_requests(user3_id, prop_u3_3to10_in1hr)))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertIn({new_requests[0], o_requests[1]},
                   potential_matches)
     self.assertEqual(len(potential_matches), 1)
 
   def test_new_later_requests_size3_match(self):
-    new_requests = tuple(ri._new_requests(user2_id, port_prop1))
-    o_requests = (tuple(ri._new_requests(admin_id, o_port_prop1_size3))
-                  + tuple(ri._new_requests(user3_id, o3_port_prop1)))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
+    o_requests = (tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr_size3))
+                  + tuple(ri._new_requests(user3_id, prop_u3_3to10_in1hr)))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertIn({new_requests[0], o_requests[0], o_requests[1]},
                   potential_matches)
     self.assertEqual(len(potential_matches), 3)
 
-    new_requests = tuple(ri._new_requests(user2_id, port_prop1))
-    o_requests = (tuple(ri._new_requests(admin_id, o_port_prop1_size3))
-                  + tuple(ri._new_requests(user3_id, o3_port_prop1))
-                  + tuple(ri._new_requests(admin_id, o_port_prop1)))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_3to10_in1hr))
+    o_requests = (tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr_size3))
+                  + tuple(ri._new_requests(user3_id, prop_u3_3to10_in1hr))
+                  + tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr)))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertIn({new_requests[0], o_requests[0], o_requests[1]},
                   potential_matches)
     self.assertEqual(len(potential_matches), 3)
 
-    new_requests = tuple(ri._new_requests(user2_id, port_prop2))
-    o_requests = (tuple(ri._new_requests(admin_id, o_port_prop1_size3))
-                  + tuple(ri._new_requests(user3_id, o3_port_prop1)))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+    o_requests = (tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr_size3))
+                  + tuple(ri._new_requests(user3_id, prop_u3_3to10_in1hr)))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertIn({new_requests[0], o_requests[0], o_requests[1]},
                   potential_matches)
     self.assertEqual(len(potential_matches), 3)
 
-    new_requests = tuple(ri._new_requests(user2_id, port_prop2))
-    o_requests = (tuple(ri._new_requests(admin_id, o_port_prop1_size3))
-                  + tuple(ri._new_requests(user3_id, o3_port_prop1))
-                  + tuple(ri._new_requests(admin_id, o_port_prop1)))
+    new_requests = tuple(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+    o_requests = (tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr_size3))
+                  + tuple(ri._new_requests(user3_id, prop_u3_3to10_in1hr))
+                  + tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr)))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertIn({new_requests[0], o_requests[0], o_requests[1]},
                   potential_matches)
     self.assertEqual(len(potential_matches), 4)
 
   def test_new_later_requests_no_size3_match(self):
-    new_requests = tuple(ri._new_requests(admin_id, o_port_prop1))
-    o_requests = (tuple(ri._new_requests(user2_id, port_prop2))
-                  + tuple(ri._new_requests(user3_id, o3_port_prop1)))
+    new_requests = tuple(ri._new_requests(admin_id, prop_uA_2to2_in1hr))
+    o_requests = (tuple(ri._new_requests(user2_id, prop_u2_2to3_in1hr_in2hr))
+                  + tuple(ri._new_requests(user3_id, prop_u3_3to10_in1hr)))
     potential_matches = rs.potential_matches(new_requests, o_requests)
     self.assertNotIn({new_requests[0], o_requests[0], o_requests[1]},
                      potential_matches)
