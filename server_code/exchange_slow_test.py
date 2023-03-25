98e2c2ff169d0865a15e3a3d753925c2ec0d6aaa
hugetim@gmail.com <hugetim@gmail.com>
2023-03-25 15:53:14 +0000

    Edited server_auto_test and exchange_slow_test

--- server_code/exchange_slow_test.py
+++ server_code/exchange_slow_test.py
@@ -13,6 +13,8 @@ from empathy_chat import requests as rs
 from empathy_chat import server_misc as sm
 from empathy_chat import portable as port
 from empathy_chat import exchanges as es
+from empathy_chat import notifies as n
+from anvil_extras.logging import TimerLogger
 
 class TestExchangeGateway(unittest.TestCase):
   def setUp(self):
@@ -20,6 +22,10 @@ class TestExchangeGateway(unittest.TestCase):
     self.request_rows_created = []
     self.exchange_records_saved = []
     self.exchange_rows_created = []
+    self._email_send = n.email_send
+    n.email_send = Mock()
+    self._send_sms = n.send_sms
+    n.send_sms = Mock()
   
   def test_exchange_record_save(self):
     prop = rt.prop_u2_2to3_now
@@ -56,26 +62,35 @@ class TestExchangeGateway(unittest.TestCase):
       self.assertEqual(saved_exchange.participants[0][key], None)
 
   def test_add_request_exchange_save(self):
-    prop1 = rt.prop_u2_2to3_in1hr_in2hr
-    prop2 = rt.prop_uA_2to2_in1hr
-    or_group1 = ri._add_request(USER2, prop1)
-    or_group2 = ri._add_request(ADMIN, prop2)
-    requests1 = list(app_tables.requests.search(or_group_id=or_group1))
-    requests2 = list(app_tables.requests.search(or_group_id=or_group2))
-    self.request_rows_created.extend(requests1 + requests2)
-    row_2hr = next((row for row in requests1 if row['start_dt'] - sm.now() > datetime.timedelta(hours=1.5)))
-    self.assertFalse(row_2hr['current'])
-    upcomings = list(eg.exchanges_by_user(USER2, records=True))
-    self.exchange_records_saved.append(upcomings[0])
-    self.assertEqual(len(upcomings), 1)
-    match_dicts2 = ei.upcoming_match_dicts(USER2)
-    match_dictsA = ei.upcoming_match_dicts(ADMIN)
-    self.assertEqual(len(match_dicts2[0]), 4)
-    for key in match_dicts2[0]:
-      if key != 'port_users':
-        self.assertEqual(match_dicts2[0][key], match_dictsA[0][key])
+    with TimerLogger("  test", format="{name}: {elapsed:6.3f} s | {msg}") as timer:
+      prop1 = rt.prop_u2_2to3_in1hr_in2hr
+      prop2 = rt.prop_uA_2to2_in1hr
+      or_group1 = ri._add_request(USER2, prop1)
+      timer.check("1st _add_request")
+      or_group2 = ri._add_request(ADMIN, prop2)
+      timer.check("2nd _add_request")
+      requests = list(app_tables.requests.search(or_group_id=q.any_of(or_group1, or_group2)))
+      self.request_rows_created.extend(requests)
+      timer.check("grab request rows created")
+      row_2hr = next((row for row in requests 
+                      if row['start_dt'] - sm.now() > datetime.timedelta(hours=1.5)))
+      self.assertFalse(row_2hr['current'])
+      timer.check("check row_2hr")
+      upcomings = list(eg.exchanges_by_user(USER2, records=True))
+      self.exchange_records_saved.append(upcomings[0])
+      self.assertEqual(len(upcomings), 1)
+      timer.check("grab upcomings")
+      match_dicts2 = ei.upcoming_match_dicts(USER2)
+      match_dictsA = ei.upcoming_match_dicts(ADMIN)
+      timer.check("grab match_dicts")
+      self.assertEqual(len(match_dicts2[0]), 4)
+      for key in match_dicts2[0]:
+        if key != 'port_users':
+          self.assertEqual(match_dicts2[0][key], match_dictsA[0][key])
     
   def tearDown(self):
+    n.email_send = self._email_send
+    n.send_sms = self._send_sms
     for row in self.request_rows_created + [rr._row for rr in self.request_records_saved]:
       row.delete()
     for row in self.exchange_rows_created + [er._row for er in self.exchange_records_saved]:
