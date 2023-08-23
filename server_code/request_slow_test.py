79d8e2a27162fa67035cdc3b19479afb78252730
hugetim@gmail.com <hugetim@gmail.com>
2023-08-23 23:52:43 +0000

    Fix rst.test_exchange_prospects_to_props

--- server_code/request_slow_test.py
+++ server_code/request_slow_test.py
@@ -221,4 +221,5 @@ class TestExchangeProspectMisc(unittest.TestCase):
     new_requests = tuple(rs.prop_to_requests(rt.prop_u2_2to3_in1hr_in2hr, with_users=[rt.admin_id]))
     o_requests = tuple(rs.prop_to_requests(rt.prop_uA_2to2_in1hr, with_users=[rt.user2_id]))
     ep = rs.ExchangeProspect({new_requests[0], o_requests[0]})
-    self.assertEqual(ri.eps_to_props([ep], USER2).user_id, rt.user2_id)
+    props = list(ri.eps_to_props([ep], USER2))
+    self.assertEqual(props[0].user.user_id, rt.user2_id)
